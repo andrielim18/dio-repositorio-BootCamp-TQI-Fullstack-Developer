aba737bdca6f50e5f1977e3a589226f75ff1f446
Andri <andrielim18@gmail.com>
2022-05-23 12:15:11 -0300

    Aula Projeto 02 - Dio - CSS

---
+++ JavaScript/indes.js
@@ -0,0 +1,20 @@
+// fazer comentários
+//criar variáveis: variáveis var são mutáveis
+//constantes: const - são imutáveis e como boa prática sempre usar letras maiusculas
+var x = 1;
+var y = 2;
+var z = x+y;
+const PRECO = 3;
+var desconto =0.2;
+total = PRECO-dessconto;
+
+//FUNÇÕES
+
+function soma(a,b){
+    //console.log(a+b) //usado para ver se algo está correto, sem precisar retornar
+    return a+b; //usado quando precisa retornar o valor da função
+}
+
+soma(3,5);
+// USANDO O CONSOLE NA WEB
+
